1d9da3487dedeae62e72b8a85f9390260b9df2a1
William Rodriguez <william@rodriguez1.com>
2022-04-05 09:06:01 -0700

    Add bussiness logic to not withdraw negative numbers
    Co-authored-by: Jeremy Martin <jeremyvs1945@gmail.com>

--- REAMDE.md
+++ REAMDE.md
@@ -7,3 +7,10 @@ Expected-Output: false
 Test: "If given a non-negative number it should return true"
 Code: deposit(1);
 Expected-Output: true
+
+
+
+Describe: withdraw()
+
+Test: "If given a negative number it should return false"
+Expected-Output: false
--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,17 @@ function BankAccount(name, deposit) {
   this.balance = deposit;
 }
 
-BankAccount.prototype.deposit = function(deposit) {
-  if (deposit >= 0) {
-    this.balance += deposit;
+BankAccount.prototype.deposit = function(amount) {
+  if (amount >= 0) {
+    this.balance += amount;
     return true;
   } else {
+    return false;
+  }
+}
+
+BankAccount.prototype.withdraw = function(amount) {
+  if (amount < 0) {
     return false;
   }
 }
